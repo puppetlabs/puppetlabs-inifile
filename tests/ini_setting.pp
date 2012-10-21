e517148f4a2ab309aeb8413095791b021574b417
Chris Price <chris@puppetlabs.com>
2012-10-20 22:16:36 -0700

    Add example for `ensure=absent`

--- tests/ini_setting.pp
+++ tests/ini_setting.pp
@@ -15,3 +15,11 @@ ini_setting { "sample setting2":
   ensure              => present,
   require             => Ini_setting["sample setting"],
 }
+
+ini_setting { "sample setting3":
+  path                => '/tmp/foo.ini',
+  section             => 'bar',
+  setting             => 'bazsetting',
+  ensure              => absent,
+  require             => Ini_setting["sample setting2"],
+}
