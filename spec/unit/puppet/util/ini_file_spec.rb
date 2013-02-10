d99c72f1da3d908eb898f4e24b2e3d5114155b86
Etienne Perot <etienne@perot.me>
2013-02-10 16:37:43 -0500

    Add rspec tests for ini_file

--- spec/unit/puppet/util/ini_file_spec.rb
+++ spec/unit/puppet/util/ini_file_spec.rb
@@ -4,14 +4,110 @@ require 'puppet/util/ini_file'
 describe Puppet::Util::IniFile do
   let(:subject) { Puppet::Util::IniFile.new("/my/ini/file/path") }
 
-  before :each do
-    File.should_receive(:file?).with("/my/ini/file/path") { true }
-    described_class.should_receive(:readlines).once.with("/my/ini/file/path") do
-      sample_content
+  context "when giving it a path to an ini file" do
+    before :each do
+      File.should_receive(:file?).with("/my/ini/file/path") { true }
+      described_class.should_receive(:readlines).once.with("/my/ini/file/path") do
+        sample_content
+      end
+    end
+
+    context "when parsing a file" do
+      let(:sample_content) {
+        template = <<-EOS
+# This is a comment
+[section1]
+; This is also a comment
+foo=foovalue
+
+bar = barvalue
+[section2]
+
+foo= foovalue2
+baz=bazvalue
+    #another comment
+ ; yet another comment
+ zot = multi word value
+        EOS
+        template.split("\n")
+      }
+
+      it "should parse the correct number of sections" do
+        # there is always a "global" section, so our count should be 3.
+        subject.section_names.length.should == 3
+      end
+
+      it "should parse the correct section_names" do
+        # there should always be a "global" section named "" at the beginning of the list
+        subject.section_names.should == ["", "section1", "section2"]
+      end
+
+      it "should expose settings for sections" do
+        subject.get_value("section1", "foo").should == "foovalue"
+        subject.get_value("section1", "bar").should == "barvalue"
+        subject.get_value("section2", "foo").should == "foovalue2"
+        subject.get_value("section2", "baz").should == "bazvalue"
+        subject.get_value("section2", "zot").should == "multi word value"
+      end
+
+    end
+
+    context "when parsing a file whose first line is a section" do
+      let(:sample_content) {
+        template = <<-EOS
+[section1]
+; This is a comment
+foo=foovalue
+        EOS
+        template.split("\n")
+      }
+
+      it "should parse the correct number of sections" do
+        # there is always a "global" section, so our count should be 2.
+        subject.section_names.length.should == 2
+      end
+
+      it "should parse the correct section_names" do
+        # there should always be a "global" section named "" at the beginning of the list
+        subject.section_names.should == ["", "section1"]
+      end
+
+      it "should expose settings for sections" do
+        subject.get_value("section1", "foo").should == "foovalue"
+      end
+
+    end
+
+    context "when parsing a file with a 'global' section" do
+      let(:sample_content) {
+        template = <<-EOS
+foo = bar
+[section1]
+; This is a comment
+foo=foovalue
+        EOS
+        template.split("\n")
+      }
+
+      it "should parse the correct number of sections" do
+        # there is always a "global" section, so our count should be 2.
+        subject.section_names.length.should == 2
+      end
+
+      it "should parse the correct section_names" do
+        # there should always be a "global" section named "" at the beginning of the list
+        subject.section_names.should == ["", "section1"]
+      end
+
+      it "should expose settings for sections" do
+        subject.get_value("", "foo").should == "bar"
+        subject.get_value("section1", "foo").should == "foovalue"
+      end
+
     end
   end
 
-  context "when parsing a file" do
+  context "when giving it the ini contents directly" do
     let(:sample_content) {
       template = <<-EOS
 # This is a comment
@@ -30,77 +126,10 @@ baz=bazvalue
       EOS
       template.split("\n")
     }
+    let(:subject_content) { Puppet::Util::IniFile.new(sample_content.join("\n"), ' = ', false) }
 
-    it "should parse the correct number of sections" do
-      # there is always a "global" section, so our count should be 3.
-      subject.section_names.length.should == 3
-    end
-
-    it "should parse the correct section_names" do
-      # there should always be a "global" section named "" at the beginning of the list
-      subject.section_names.should == ["", "section1", "section2"]
-    end
-
-    it "should expose settings for sections" do
-      subject.get_value("section1", "foo").should == "foovalue"
-      subject.get_value("section1", "bar").should == "barvalue"
-      subject.get_value("section2", "foo").should == "foovalue2"
-      subject.get_value("section2", "baz").should == "bazvalue"
-      subject.get_value("section2", "zot").should == "multi word value"
-    end
-
-  end
-
-  context "when parsing a file whose first line is a section" do
-    let(:sample_content) {
-      template = <<-EOS
-[section1]
-; This is a comment
-foo=foovalue
-      EOS
-      template.split("\n")
-    }
-
-    it "should parse the correct number of sections" do
-      # there is always a "global" section, so our count should be 2.
-      subject.section_names.length.should == 2
-    end
-
-    it "should parse the correct section_names" do
-      # there should always be a "global" section named "" at the beginning of the list
-      subject.section_names.should == ["", "section1"]
-    end
-
-    it "should expose settings for sections" do
-      subject.get_value("section1", "foo").should == "foovalue"
-    end
-
-  end
-
-  context "when parsing a file with a 'global' section" do
-    let(:sample_content) {
-      template = <<-EOS
-foo = bar
-[section1]
-; This is a comment
-foo=foovalue
-      EOS
-      template.split("\n")
-    }
-
-    it "should parse the correct number of sections" do
-      # there is always a "global" section, so our count should be 2.
-      subject.section_names.length.should == 2
-    end
-
-    it "should parse the correct section_names" do
-      # there should always be a "global" section named "" at the beginning of the list
-      subject.section_names.should == ["", "section1"]
-    end
-
-    it "should expose settings for sections" do
-      subject.get_value("", "foo").should == "bar"
-      subject.get_value("section1", "foo").should == "foovalue"
+    it "should return the same lines as when it reads a file" do
+      subject_content.send(:lines).should == sample_content
     end
 
   end
