c51097203daff52d07f1413ce4685ec6d68dc65b
Ivanov Aleksey <ival.wrk@gmail.com>
2013-07-08 18:25:08 +0400

    support for whitespaces in settings names (need for smb.conf)

--- lib/puppet/util/ini_file.rb
+++ lib/puppet/util/ini_file.rb
@@ -6,7 +6,7 @@ module Util
   class IniFile
 
     @@SECTION_REGEX = /^\s*\[([\w\d\.\\\/\-\:]+)\]\s*$/
-    @@SETTING_REGEX = /^(\s*)([\w\d\.\\\/\-]+)(\s*=\s*)([\S\s]*\S)\s*$/
+    @@SETTING_REGEX = /^(\s*)([\w\d\.\\\/\-\s]*[\w\d\.\\\/\-])(\s*=\s*)([\S\s]*\S)\s*$/
     @@COMMENTED_SETTING_REGEX = /^(\s*)[#;]+(\s*)([\w\d\.\\\/\-]+)(\s*=\s*)([\S\s]*\S)\s*$/
 
     def initialize(path, key_val_separator = ' = ')
