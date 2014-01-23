6fb1b356d9cb23c5f90190ef6d670574a5bbe772
Garrett Honeycutt <code@garretthoneycutt.com>
2014-01-23 18:38:13 -0500

    Clean up spec_helper.rb to allow tests to pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,4 @@
 require 'rubygems'
-
-gem 'rspec', '>=2.0.0'
-require 'rspec/expectations'
-
-
-require 'puppetlabs_spec_helper/puppetlabs_spec_helper'
-
-require 'puppetlabs_spec_helper/puppetlabs_spec/files'
-
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
