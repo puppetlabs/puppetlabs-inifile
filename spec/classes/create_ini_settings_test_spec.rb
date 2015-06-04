44f40f6f03cfb1c4df34683f127042580774d0ae
Jonathan Tripathy <jt@puppetlabs.com>
2015-06-04 14:27:59 +0100

    Updated travisci file to remove allow_failures on Puppet 4

--- spec/classes/create_ini_settings_test_spec.rb
+++ spec/classes/create_ini_settings_test_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 # end-to-end test of the create_init_settings function
 describe 'create_ini_settings_test' do
   it { should have_ini_setting_resource_count(3) }
-  it { should contain_ini_setting('[section1] setting1').with(
+  it { should contain_ini_setting('[section1] setting10').with(
     :ensure  => 'present',
     :section => 'section1',
-    :setting => 'setting1',
+    :setting => 'setting10',
     :value   => 'val1',
     :path    => '/tmp/foo.ini'
   )}
-  it { should contain_ini_setting('[section2] setting2').with(
+  it { should contain_ini_setting('[section2] setting20').with(
     :ensure  => 'present',
     :section => 'section2',
-    :setting => 'setting2',
+    :setting => 'setting20',
     :value   => 'val2',
     :path    => '/tmp/foo.ini'
   )}
-  it { should contain_ini_setting('[section2] setting3').with(
+  it { should contain_ini_setting('[section2] setting30').with(
     :ensure  => 'absent',
     :section => 'section2',
-    :setting => 'setting3',
+    :setting => 'setting30',
     :path    => '/tmp/foo.ini'
   )}
 end
