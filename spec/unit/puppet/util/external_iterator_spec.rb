e2bbc904ed28c8bb53776004d7b3da9be14f268e
Will Meek <william.meek@puppet.com>
2017-12-13 16:09:43 +0000

    Fixes rubocop issues found by modulesync

--- spec/unit/puppet/util/external_iterator_spec.rb
+++ spec/unit/puppet/util/external_iterator_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 require 'puppet/util/external_iterator'
 
 describe Puppet::Util::ExternalIterator do
-  let(:subject) { described_class.new(%w[a b c]) }
+  subject_class = nil
+  expected_values = nil
 
-  expected_values = [['a', 0], ['b', 1], ['c', 2]]
+  before(:each) do
+    subject_class = described_class.new(%w[a b c])
+    expected_values = [['a', 0], ['b', 1], ['c', 2]]
+  end
 
   context '#next' do
     it 'iterates over the items' do
       expected_values.each do |expected_pair|
-        expect(subject.next).to eq(expected_pair)
+        expect(subject_class.next).to eq(expected_pair)
       end
     end
   end
@@ -17,15 +21,15 @@ describe Puppet::Util::ExternalIterator do
   context '#peek' do
     it 'returns the 0th item repeatedly' do
       (0..2).each do |_i|
-        expect(subject.peek).to eq(expected_values[0])
+        expect(subject_class.peek).to eq(expected_values[0])
       end
     end
 
     it 'does not advance the iterator, but should reflect calls to #next' do # rubocop:disable RSpec/MultipleExpectations : No neat way to further reduce expectations
       expected_values.each do |expected_pair|
-        expect(subject.peek).to eq(expected_pair)
-        expect(subject.peek).to eq(expected_pair)
-        expect(subject.next).to eq(expected_pair)
+        expect(subject_class.peek).to eq(expected_pair)
+        expect(subject_class.peek).to eq(expected_pair)
+        expect(subject_class.next).to eq(expected_pair)
       end
     end
   end
