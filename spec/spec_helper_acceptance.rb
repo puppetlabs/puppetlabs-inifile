b91fc2aa566c856e37df695ff92074f09b431e3a
lionce <loredana.ionce@puppet.com>
2018-10-02 10:17:25 +0300

    (FM-7331) - i18N-ify

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,6 +4,8 @@ require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
+require 'beaker/i18n_helper'
+require 'beaker-task_helper'
 
 run_puppet_install_helper
 configure_type_defaults_on(hosts)
@@ -18,4 +20,29 @@ RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
   c.treat_symbols_as_metadata_keys_with_true_values = true
+
+  c.before :suite do
+    run_puppet_access_login(user: 'admin') if pe_install? && (Gem::Version.new(puppet_version) >= Gem::Version.new('5.0.0'))
+    hosts.each do |host|
+      # This will be removed, this is temporary to test localisation.
+      if (fact('osfamily') == 'Debian' || fact('osfamily') == 'RedHat') &&
+         (Gem::Version.new(puppet_version) >= Gem::Version.new('4.10.5') &&
+          Gem::Version.new(puppet_version) < Gem::Version.new('5.2.0'))
+        on(host, 'mkdir /opt/puppetlabs/puppet/share/locale/ja')
+        on(host, 'touch /opt/puppetlabs/puppet/share/locale/ja/puppet.po')
+      end
+      if fact('osfamily') == 'Debian'
+        # install language on debian systems
+        install_language_on(host, 'ja_JP.utf-8') if not_controller(host)
+        # This will be removed, this is temporary to test localisation.
+      end
+      # Required for binding tests.
+      if fact('osfamily') == 'RedHat'
+        if fact('operatingsystemmajrelease') =~ %r{7} || fact('operatingsystem') =~ %r{Fedora}
+          shell('yum install -y bzip2')
+        end
+      end
+      on host, puppet('module', 'install', 'stahnma/epel')
+    end
+  end
 end
