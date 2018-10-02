8cdf832e3eab28139d01c399df761f3ceb7adb96
lionce <loredana.ionce@puppet.com>
2018-10-02 11:37:09 +0300

    úpdated some decorators

--- lib/puppet/parser/functions/create_ini_settings.rb
+++ lib/puppet/parser/functions/create_ini_settings.rb
@@ -63,7 +63,7 @@ module Puppet::Parser::Functions
     resources = settings.keys.each_with_object({}) do |section, res|
       unless settings[section].is_a?(Hash)
         raise(Puppet::ParseError,
-              _('create_ini_settings(): Section %{section} must contain a Hash')) % { section: section }
+              _('create_ini_settings(): Section #{section} must contain a Hash'))
       end
 
       path = defaults.merge(settings)['path']
