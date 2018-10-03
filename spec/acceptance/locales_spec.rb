337cf2a491d96c1a8441b6c1073d7ab3a4bbb401
lionce <loredana.ionce@puppet.com>
2018-10-03 14:23:11 +0300

    updated i18N test

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper_acceptance'
 require 'beaker/i18n_helper'
+require 'pry'
+
+tmpdir = default.tmpdir('tmp')
 
 describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') == 'RedHat') && (Gem::Version.new(puppet_version) >= Gem::Version.new('4.10.5')) do
   before :all do
     hosts.each do |host|
       on(host, "sed -i \"96i FastGettext.locale='ja'\" /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet.rb")
       change_locale_on(host, 'ja_JP.utf-8')
+    end
+  end
+
+  shared_examples 'has_error' do |path, pp, error|
+    before :all do
+      shell("rm #{path}", acceptable_exit_codes: [0, 1, 2])
+    end
+    after :all do
+      shell("cat #{path}", acceptable_exit_codes: [0, 1, 2])
+      shell("rm #{path}", acceptable_exit_codes: [0, 1, 2])
+    end
+
+    it 'applies the manifest and gets a failure message' do
+      expect(apply_manifest(pp, expect_failures: true).stderr).to match(error)
+      binding.pry
+    end
+
+    describe file(path) do
+      it { is_expected.not_to be_file }
     end
   end
 
@@ -20,10 +42,9 @@ describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') =
       }
     EOS
 
-    apply_manifest(pp, catch_failures: true) do |r|
-      expect(r.stderr).to match(%r{Ƒīŀḗ ƥȧŧħş ḿŭşŧ ƀḗ ƒŭŀŀẏ ɋŭȧŀīƒīḗḓ, ƞǿŧ})
+    it_behaves_like 'has_error', 'foo', pp, %r{Ƒīŀḗ ƥȧŧħş ḿŭşŧ ƀḗ ƒŭŀŀẏ ɋŭȧŀīƒīḗḓ, ƞǿŧ}
     end
-  end
+
 
   after :all do
     hosts.each do |host|
