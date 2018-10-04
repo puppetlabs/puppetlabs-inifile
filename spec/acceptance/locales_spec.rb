cffde4e1b0930cdbc9626179793c9765dd98f3e6
lionce <loredana.ionce@puppet.com>
2018-10-04 16:24:33 +0300

    added test for interpolated Japanese message

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper_acceptance'
 require 'beaker/i18n_helper'
+require 'rspec-puppet'
+require 'spec_helper'
 
 describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') == 'RedHat') && (Gem::Version.new(puppet_version) >= Gem::Version.new('4.10.5')) do
   before :all do
+    Puppet::Parser::Functions.autoloader.loadall
+    Puppet::Parser::Functions.function(:create_resources)
     hosts.each do |host|
       on(host, "sed -i \"96i FastGettext.locale='ja'\" /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet.rb")
       change_locale_on(host, 'ja_JP.utf-8')
@@ -41,6 +45,10 @@ describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') =
     it_behaves_like 'has_error', 'foo', pp, %r{Ƒīŀḗ ƥȧŧħş ḿŭşŧ ƀḗ ƒŭŀŀẏ ɋŭȧŀīƒīḗḓ, ƞǿŧ}
   end
 
+  describe 'display interpolated Japanise message' do
+    it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{0 ƒǿř 1 ǿř 2}) }
+  end
+
   after :all do
     hosts.each do |host|
       on(host, 'sed -i "96d" /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet.rb')
@@ -48,3 +56,4 @@ describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') =
     end
   end
 end
+
