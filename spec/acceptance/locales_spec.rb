637844f7f08c22f530dcb37605064701de7d9c6a
lionce <loredana.ionce@puppet.com>
2018-10-04 18:01:18 +0300

    updated locales_spec

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -1,12 +1,10 @@
+require 'spec_helper'
 require 'spec_helper_acceptance'
 require 'beaker/i18n_helper'
-require 'rspec-puppet'
-require 'spec_helper'
+require 'pry'
 
 describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') == 'RedHat') && (Gem::Version.new(puppet_version) >= Gem::Version.new('4.10.5')) do
   before :all do
-    Puppet::Parser::Functions.autoloader.loadall
-    Puppet::Parser::Functions.function(:create_resources)
     hosts.each do |host|
       on(host, "sed -i \"96i FastGettext.locale='ja'\" /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet.rb")
       change_locale_on(host, 'ja_JP.utf-8')
@@ -25,13 +23,9 @@ describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') =
     it 'applies the manifest and gets a failure message' do
       expect(apply_manifest(pp, expect_failures: true).stderr).to match(error)
     end
-
-    describe file(path) do
-      it { is_expected.not_to be_file }
-    end
   end
 
-  context 'path => foo' do
+  context 'display Japanese error' do
     pp = <<-EOS
       ini_setting { 'path => foo':
         ensure     => present,
@@ -45,8 +39,22 @@ describe 'i18n Testing', if: (fact('osfamily') == 'Debian' || fact('osfamily') =
     it_behaves_like 'has_error', 'foo', pp, %r{Ƒīŀḗ ƥȧŧħş ḿŭşŧ ƀḗ ƒŭŀŀẏ ɋŭȧŀīƒīḗḓ, ƞǿŧ}
   end
 
-  describe 'display interpolated Japanise message' do
-    it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{0 ƒǿř 1 ǿř 2}) }
+  context 'display interpolated Japanese error' do
+    pp = <<-EOS
+      ini_setting { 'path => test':
+        ensure     => present,
+        section    => 'section',
+        setting    => 'setting',
+        value      => 'three',
+        path       => 'test',
+      }
+      EOS
+
+      it 'displays Japanese failure' do
+        apply_manifest(pp, expect_failures: true) do |r|
+        expect(r.stderr).to match(%r{Ƒīŀḗ ƥȧŧħş ḿŭşŧ ƀḗ ƒŭŀŀẏ ɋŭȧŀīƒīḗḓ, ƞǿŧ '%{value}'})
+      end
+    end
   end
 
   after :all do
